e373f4d302a1dcc0aa0839b217e0b3fe5d8aff6d
Dian M Fay <dian.m.fay@gmail.com>
2021-12-27 13:28:35 -0500

    test: another reference

--- test/test.sql
+++ test/test.sql
@@ -4,4 +4,11 @@ SELECT * FROM myview;
 SELECT id, val, val2 FROM one;
 SELECT id, val, an_int FROM one;
 
+UPDATE one SET val = 'hi';
+
+select one.id, t.val
+from one
+join two as t on t.one_id = one.id
+where one.an_int > 0;
+
 SELECT * from two;
